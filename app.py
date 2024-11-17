7397a8a65f996ef2987173310b500a321e78acfe
bxlionelle <b.lionellejayne@gamil.com>
2024-11-17 18:15:43 +0800

    BOOKING NALANG

--- app.py
+++ app.py
@@ -47,6 +47,38 @@ class Home:
     @app.route("/")
     def home():
         return render_template('home.html', rooms=rooms)
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        get_email = request.form.get("email")
+        get_password = request.form.get("password")
+        con = sqlite3.connect(currentdirectory + '\data.db')
+        c = con.cursor()
+        query1 = "SELECT * FROM GUEST WHERE email = ? AND password = ?"
+        c.execute(query1, (get_email, get_password))
+        guest = c.fetchone()  
+        
+        if guest:
+            session.clear()
+            
+            session['user_info'] = {
+                'firstname': guest[1],  
+                'lastname': guest[2],   
+                'gender': guest[3],     
+                'cel_num': guest[4],    
+                'address': guest[5],    
+                'email': guest[6],      
+            }
+            session['email'] = guest[6]  # ig check an email kun uniq
+            con.close()
+            return redirect(url_for("index"))
+        else:
+            con.close()
+            return render_template('login.html', message="Invalid email or password")
+    else:
+        return render_template('login.html')
+
     
 @app.route('/signup', methods=["GET", "POST"])
 def signup():
@@ -61,51 +93,50 @@ def signup():
             'password': request.form.get("password")
         }
 
-        guestlist.append(customer)
-        session['guestlist'] = guestlist  # Update session with the latest guestlist
-        
-        session['user_info'] = customer  # Log in the user
-        print(guestlist)
-        
-        return redirect(url_for('login'))
-    else: 
+        con = sqlite3.connect(currentdirectory + '\data.db')
+        c = con.cursor()
+
+        email = customer['email']
+        query = "SELECT * FROM GUEST WHERE email = ?"
+        c.execute(query, (email,))
+        guest = c.fetchone()
+
+        if guest:
+            con.close()
+            return render_template('sign_up.html', message="Email already exists. Please use a different email.")
+        else:
+            # ma proceded dari
+            query = """
+                INSERT INTO GUEST (firstname, lastname, gender, cel_num, address, email, password) 
+                VALUES (?, ?, ?, ?, ?, ?, ?)
+            """
+            c.execute(query, (
+                customer['firstname'], 
+                customer['lastname'], 
+                customer['gender'], 
+                customer['cel_num'], 
+                customer['address'], 
+                customer['email'], 
+                customer['password']
+            ))
+            con.commit()
+            con.close()
+
+            # igsession an user_info(customer)
+            session['user_info'] = customer
+            return redirect(url_for('login'))
+    else:
         return render_template('sign_up.html')
 
 
-
-@app.route('/login', methods=["GET", "POST"])
-def login():
-    if request.method == "POST":
-        get_email = request.form.get("email")
-        get_password = request.form.get("password")
-        valid = False
-        
-        user_info = session.get('user_info') #kuwaon an nakadto sulod han session var which is user_info(customer)
-        
-        for customer in guestlist: 
-           if get_email == customer['email'] and get_password == customer['password']:
-                valid = True
-                session['user_info'] = {
-                    'firstname': customer['firstname'],
-                    'lastname': customer['lastname'],
-                    'gender': customer['gender'],
-                    'cel_num': customer['cel_num'],
-                    'address': customer['address'],
-                    'email': customer['email'],
-                }
-                session['email'] = get_email
-                break
-        
-        if valid:
-            session['email'] = get_email
-            return render_template("index.html", rooms=rooms, user_info=user_info)
-        else:
-            return render_template('login.html', message="Invalid credentials")
-    
-    return render_template('login.html')
-
 @app.route("/edit", methods=["GET", "POST"])
 def edit():
+    if 'user_info' not in session:
+        flash("You need to log in to edit your profile.")
+        return redirect(url_for('login'))
+
+    user_info = session['user_info']
+
     if request.method == "POST":
         updated_user_info = {
             "firstname": request.form.get("first-name"),
@@ -115,29 +146,58 @@ def edit():
             "cel_num": request.form.get("cel-number"),
         }
 
-        # Update session data
-        session['user_info'] = updated_user_info #ig convert niya an updated_user_info tikadto ha session var na user_info(customer)
+        session['user_info'].update(updated_user_info)
 
-        # Update the corresponding user in guestlist
-        for user in guestlist:
-            if user['email'] == session['user_info']['email']:
-                user.update(updated_user_info)
-                break
+        con = sqlite3.connect(currentdirectory + '\data.db')
+        c = con.cursor()
+        query = """
+            UPDATE GUEST
+            SET firstname = ?, lastname = ?, address = ?, cel_num = ?, email = ?
+            WHERE email = ?
+        """
+        c.execute(query, (
+            updated_user_info["firstname"],
+            updated_user_info["lastname"],
+            updated_user_info["address"],
+            updated_user_info["cel_num"],
+            updated_user_info["email"],
+            user_info["email"]
+        ))
+        con.commit()
+        con.close()
 
+        flash("Profile updated successfully!")
         return redirect(url_for('profile'))
 
-    return render_template("edit.html", user_info=session.get('user_info'))
+    return render_template("edit.html", user_info=user_info)
+
 
 @app.route('/profile', methods=["GET"])
 def profile():
     if 'user_info' not in session:
         flash("You need to log in to view your profile.")
         return redirect(url_for('login'))
-    
-    user_info = session.get('user_info') #ig show info nga nakadto to user_info(customer) nga in session
-    
-    #AN TRANSACTIONS NALA, LIKE MAKITA HIRA KUN ANO AN IRA GIN BOOK
-    return render_template("profile.html", user_info=user_info)
+
+    user_email = session['user_info']['email']
+    con = sqlite3.connect(currentdirectory + '\data.db')
+    c = con.cursor()
+    query = "SELECT firstname, lastname, gender, cel_num, address, email FROM GUEST WHERE email = ?"
+    c.execute(query, (user_email,))
+    updated_info = c.fetchone()
+    con.close()
+
+    if updated_info:
+        session['user_info'] = {
+            "firstname": updated_info[0],
+            "lastname": updated_info[1],
+            "gender": updated_info[2],
+            "cel_num": updated_info[3],
+            "address": updated_info[4],
+            "email": updated_info[5],
+        }
+
+    return render_template("profile.html", user_info=session['user_info'])
+
 
 
 @app.route("/index", methods=["GET", "POST"])
@@ -184,7 +244,7 @@ class Rent:
             
             user_info = session.get('user_info')
             
-            session['guestlist'] = guestlist  # Update the session with the modified guestlist
+            session['guestlist'] = guestlist 
 
             return redirect(url_for('payment', guest_index=len(guestlist) - 1))
 
@@ -238,7 +298,6 @@ def logout():
         session['user_info'] = {key: session['user_info'][key] for key in session['user_info'] if key not in ['email']}
     session.pop('email', None)
     return redirect(url_for('home'))
-
 
 
 
